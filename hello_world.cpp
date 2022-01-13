d1c8f588077925bab168f7de19b991032fc23ef1
Kristian <kristianedwardlewis@gmail.com>
2022-01-13 18:56:41 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   hello_world.cpp
    
     first commit test

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+//testing termux on kindle fire 8 (2017) with lineageos installed
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
