4f23c77b87ae48b2d19951e4263fad4684f66153
Kristian <kristianedwardlewis@gmail.com>
2022-01-20 13:15:03 -0500

    Changes to be committed: 	modified:   insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -16,6 +16,22 @@ int main()
 	cout << "read test done" << endl;
 	testing output*/ 
 	
-	
+
+	int key;
+	for(int i=1; i < seq.size; i++)
+	{
+		key = seq.array[i];
+	       	int j = i;
+		while(j > 0 && seq.array[j-1]>key)
+		{
+			j--;
+			seq.array[j+1] = seq.array[j];
+			
+		}	
+		seq.array[j] = key;
+	}
+
+
+
 	write_file(seq, "test2.txt");	
 }
